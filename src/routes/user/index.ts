dcf9d89726803758f1ebdc0aa6e5d8b2bf8ff095
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2021-12-27 19:31:12 +0000

    validating input data

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -2,11 +2,23 @@ import { Router } from 'express'
 
 import UserController from '@controllers/UserController'
 import auth from 'src/middlewares/auth'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 const routes = Router()
 const userController = new UserController()
 
-routes.post('/', userController.create)
+routes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().max(50).required(),
+      email: Joi.string().max(70).email().required(),
+      password: Joi.string().min(6).max(60).required(),
+      cpf: Joi.string().length(11).required(),
+    },
+  }),
+  userController.create
+)
 
 routes.use(auth)
 routes.get('/me', userController.show)
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import 'dotenv/config'
 import '@config/db'
+import { errors } from 'celebrate'
 import routes from './routes'
 import errorHandler from './middlewares/errorHandler'
 
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use(errors())
 app.use(errorHandler)
 
 app.listen(3000, () => {
